f4aad90541ea5e191485f092772602a2ebd8283d
Stefan van Groningen <groninge@hotmail.com>
2017-10-11 19:55:29 +0200

    Modified remove li to work with the new styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 $(function() {
     $('#add').click(function() {
         var listvalue = $('#textbox').val();
-                $("#textbox").val("");
-            $('ul').append('<li class="ui-widget ui-state-default ui-corner-all list-item padded">' + listvalue + '<a href="#">x</a></li>');
+            $("#textbox").val("");
+            $('ul').append('<li class="ui-widget ui-state-default ui-corner-all list-item padded"><div class="flex-wrapper"><div>' + listvalue + '</div><div><a href="#">x</a></div></div></li>');
     });
 
     $('#textbox').keypress(function(event) {
         if(event.which === 13) {
             var listvalue = $('#textbox').val();
-                $('#textbox').val("");
-                $('ul').append('<li class="ui-widget ui-state-default ui-corner-all list-item padded">' + listvalue + '<a href="#">x</a></li>');
+            $('#textbox').val("");
+            $('ul').append('<li class="ui-widget ui-state-default ui-corner-all list-item padded"><div class="flex-wrapper"><div>' + listvalue + '</div><div><a href="#">x</a></div></div></li>');
         }
     });
 
-    $('#list').sortable();
-    $('#list').disableSelection();
-     });
+    $( "#list" ).sortable();
+    $( "#list" ).disableSelection();
+
+    $('#list').delegate('a', 'click', function(e) {
+        $(this).parent().parent().parent().remove();
+    });
+
 });
 
