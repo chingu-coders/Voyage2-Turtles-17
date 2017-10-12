8742ffa9e94121d8e4d9bfc8f27c0adb3b7314c0
Stefan van Groningen <groninge@hotmail.com>
2017-10-12 20:21:48 +0200

    Added some variables and comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,34 @@
 $(function() {
+    var $newItem = $('#textbox'),
+        $todoList = $('#list');
+
+    // click button to add item
     $('#add').click(function() {
         $.publish('/todo/add', []);
     });
 
-    $('#textbox').keypress(function(event) {
+    // press enter to add item
+    $newItem.keypress(function(event) {
         if(event.which === 13) {
             $.publish('/todo/add', []);
         }
     });
 
+    // click X to remove item from list
+    $todoList.delegate('a', 'click', function() {
+        $(this).parent().parent().parent().remove(); // a -> div -> div -> li
+    });
+
+    // drag & drop items to rearrange them
+    $todoList.sortable();
+    $todoList.disableSelection();
+
+    // Subscriptions
+
+    // add subscription
     $.subscribe('/todo/add', function() {
         if ($newItem.val() != "") {
-
+            
             $('ul').append('<li class="ui-widget ui-state-default ui-corner-all list-item padded"><div class="flex-wrapper"><div>' + $newItem.val() + '</div><div><a href="#">x</a></div></div></li>');
 
             $newItem.val("");
